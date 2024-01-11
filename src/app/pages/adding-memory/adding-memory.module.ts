a7aad9e448aee9d71f01551654decb89d2f40c6c
Flo <florianhofer024@gmail.com>
2024-01-11 17:10:54 +0100

    first stage of file upload

--- src/app/pages/adding-memory/adding-memory.module.ts
+++ src/app/pages/adding-memory/adding-memory.module.ts
@@ -11,7 +11,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import {MatStepperModule} from '@angular/material/stepper'; 
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { ImageUploadComponent } from '../../components/image-upload/image-upload.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar'; 
+import { MatListModule } from '@angular/material/list';
 const routes: Routes = [
   {
     path: '',
@@ -22,6 +24,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AddingMemoryComponent,
+    ImageUploadComponent,
   ],
   imports: [
     CommonModule,
@@ -34,7 +37,9 @@ const routes: Routes = [
     MatInputModule,
     MatButtonModule,
     MatStepperModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressBarModule,
+    MatListModule
   ]
 })
 export class AddingMemoryModule {}

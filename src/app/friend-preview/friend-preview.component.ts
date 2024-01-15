51ebeffee84aa1d1476b240f055dc878c26acc76
Flo <florianhofer024@gmail.com>
2024-01-15 18:08:48 +0100

    friend preview changes

--- src/app/friend-preview/friend-preview.component.ts
+++ src/app/friend-preview/friend-preview.component.ts
@@ -7,6 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class FriendPreviewComponent {
   @Input() requested: boolean = false;
+  @Input() buttonText: string = 'Request';
+  @Input() requestedText: string = 'Requested';
+  @Input() buttonColor: string = 'primary'; // Use MatButton color options (primary, accent, warn, etc.)
+  @Input() buttonIcon: string = 'person_add'; // Use MatButton icon options
 
   requestFriend() {
     this.requested = !this.requested;

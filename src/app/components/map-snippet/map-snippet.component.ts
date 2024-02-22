adc0e9f18d21f8fb5eaa19444d6df914ae1554e5
Flo <florianhofer024@gmail.com>
2024-02-22 17:43:29 +0100

    map gets centered correctly

--- src/app/components/map-snippet/map-snippet.component.ts
+++ src/app/components/map-snippet/map-snippet.component.ts
@@ -24,24 +24,12 @@ export class MapSnippetComponent {
   markerOptions: google.maps.MarkerOptions = {draggable: false};
 
   mapCenter: google.maps.LatLng | undefined;
-  markerPosition!: google.maps.LatLngLiteral;
+  markerPosition!: google.maps.LatLng;
 
   ngOnInit() {
-    var lat = parseInt(this.lat);
-    var lng = parseInt(this.long);
-    lat = 10;
-    this.mapCenter = new google.maps.LatLng(lat, lng)
-  }
-
-  private updateMap() {
-    if (this.lat && this.long) {
-      const lat = parseFloat(this.lat);
-      const lng = parseFloat(this.long);
-
-      // Check if parsing is successful
-      if (!isNaN(lat) && !isNaN(lng)) {
-        console.log(lat, lng);
-      }
-    }
+    const lat: number = parseFloat(this.lat);
+    const lng: number = parseFloat(this.long);
+    this.mapCenter = (new google.maps.LatLng(lng, lat));
+    this.markerPosition = (new google.maps.LatLng(lng, lat));
   }
 }

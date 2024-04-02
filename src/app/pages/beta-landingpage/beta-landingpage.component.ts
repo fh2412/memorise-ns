a5c86015fc358c3eb728c4434d25f3c8b0423e12
Flo <florianhofer024@gmail.com>
2024-04-02 18:07:59 +0200

    finalised beta landing page

--- src/app/pages/beta-landingpage/beta-landingpage.component.ts
+++ src/app/pages/beta-landingpage/beta-landingpage.component.ts
@@ -9,9 +9,33 @@ import { BetaUserService } from '../../services/user-registration.service';
   styleUrl: './beta-landingpage.component.scss'
 })
 export class BetaLandingpageComponent {
+  betaTitles: string[] = [
+    'We\'re in Beta!',
+    'Memorise is in Early Access.',
+    'Experience Memorise Before Anyone Else.',
+    'Memorise: Coming Soon, Better Now (in Beta).',
+    'Unlock the Power of Memorise (Beta).'
+  ];
+  betaMessages: string[] = [
+    'Join us as we build something special. Sign up for early access to Memorise.',
+    'Get a sneak peek and help us shape the future of memoriseing. Sign up now!',
+    'Our Beta is live! Secure your spot and start memorizing effectively.',
+    'Be among the first to try our innovative memorization tool. Sign up for Beta access.',
+    'We\'re putting Memorise to the test – join our Beta and help us refine the future of adventure finding'
+  ];
+  randomBetaTitle: string = "";
+  randomBetaMessage: string = "";
+
+  ngOnInit() {
+    this.randomBetaTitle = this.betaTitles[Math.floor(Math.random() * this.betaTitles.length)];
+    this.randomBetaMessage = this.betaMessages[Math.floor(Math.random() * this.betaMessages.length)];
+  }
+  
   email = new FormControl('', [Validators.required, Validators.email]);
 
   constructor(private router: Router, private request: BetaUserService) {}
+
+
 
   onSubmit() {
     if(this.email?.value){

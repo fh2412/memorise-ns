72065a4ba54b1f9e32a8d1acd6847c080027f448
Flo <florianhofer024@gmail.com>
2024-04-02 18:22:20 +0200

    added more text variants to the landing page

--- src/app/pages/beta-landingpage/beta-landingpage.component.ts
+++ src/app/pages/beta-landingpage/beta-landingpage.component.ts
@@ -14,14 +14,23 @@ export class BetaLandingpageComponent {
     'Memorise is in Early Access.',
     'Experience Memorise Before Anyone Else.',
     'Memorise: Coming Soon, Better Now (in Beta).',
-    'Unlock the Power of Memorise (Beta).'
+    'Unlock the Power of Memorise (Beta).',
+    'Never let those special moments fade away.',
+    'Relive the magic of shared experiences.',
+    'Make every gathering a memory that lasts.',
+    'Turn fleeting moments into lasting memories.',
+    'Building a memory bank for your happiest moments.'
   ];
   betaMessages: string[] = [
     'Join us as we build something special. Sign up for early access to Memorise.',
     'Get a sneak peek and help us shape the future of memoriseing. Sign up now!',
     'Our Beta is live! Secure your spot and start memorizing effectively.',
     'Be among the first to try our innovative memorization tool. Sign up for Beta access.',
-    'We\'re putting Memorise to the test – join our Beta and help us refine the future of adventure finding'
+    'We\'re putting Memorise to the test – join our Beta and help us refine the future of adventure finding',
+    'Join the Memorise Beta and treasure them forever. Capture and cherish the laughter, love, and joy you share with friends and family.',
+    'Try the Memorise Beta to keep those precious memories close. Create a lasting memory bank and revisit the joy of moments with loved ones.',
+    'Sign up for the Memorise Beta and capture the beauty of time spent with friends and family. Don\'t miss a chance to cherish those special moments forever.',
+    'Join the Memorise Beta! Our innovative tool helps you treasure the joy of shared experiences. Keep those beautiful moments close and relive them whenever you wish.',
   ];
   randomBetaTitle: string = "";
   randomBetaMessage: string = "";

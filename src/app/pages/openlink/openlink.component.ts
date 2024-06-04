baa35d20569e3cdd50a6709b83f414c690280334
Flo <florianhofer024@gmail.com>
2024-06-04 19:55:46 +0200

    adding friends via link works

--- src/app/pages/openlink/openlink.component.ts
+++ src/app/pages/openlink/openlink.component.ts
@@ -13,14 +13,16 @@ import { FriendsService } from '../../services/friends.service';
 export class OpenlinkComponent implements OnInit {
   userId: any;
   user: any;
+  loggedInUserId: any;
 
-  constructor(private route: ActivatedRoute, public dialog: MatDialog, private userServie: UserService, private friedService: FriendsService) {
+  constructor(private route: ActivatedRoute, public dialog: MatDialog, private userService: UserService, private friedService: FriendsService) {
     
   }
 
   async ngOnInit() {
     this.userId = this.route.snapshot.paramMap.get('userId');
-    this.userServie.getUser(this.userId).subscribe(
+    this.loggedInUserId = this.userService.getLoggedInUserId();
+    this.userService.getUser(this.userId).subscribe(
       (response) => {
         this.user = response;
         this.showConfirmDialog();
@@ -44,9 +46,21 @@ export class OpenlinkComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        // Logic to add friend
-        console.log("Send Friend Request")
+        this.friedService.sendFriendRequest( this.loggedInUserId, this.userId).subscribe(
+          response => {
+            console.log('Friend request sent successfully', response);
+          },
+          error => {
+            console.error('Error sending friend request', error);
+          }
+        );
       }
     });
   }
 }
+
+
+
+//TODO
+//Redirect to home if the userID does not exist
+//Check why the dialog has to be close twice

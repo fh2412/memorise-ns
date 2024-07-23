199c7371394411e0c45a730334b24f3374e428cb
Flo <florianhofer024@gmail.com>
2024-07-23 16:58:47 +0200

    different between owner and employee

--- src/app/components/company-information/company-information.component.ts
+++ src/app/components/company-information/company-information.component.ts
@@ -6,9 +6,20 @@ import { Component, Input } from '@angular/core';
   styleUrl: './company-information.component.scss'
 })
 export class CompanyInformationComponent {
+
   @Input() company: any;
+  @Input() userId: any;
 
   changeCompanyDetails() {
     throw new Error('Method not implemented.');
+  }
+
+  leaveOrDeleteCompany(deleteBool: boolean) {
+    if(deleteBool){
+      console.log("delete")
     }
+    else{
+      console.log("leave")
+    }
+  }
 }

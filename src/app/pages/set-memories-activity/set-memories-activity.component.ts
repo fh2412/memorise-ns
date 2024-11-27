802fbda7b72d3591e3963a2211405c84a8dc8630
Flo <florianhofer024@gmail.com>
2024-11-27 18:09:26 +0100

    created add acitivty to memory UI

---
+++ src/app/pages/set-memories-activity/set-memories-activity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-memories-activity',
+  templateUrl: './set-memories-activity.component.html',
+  styleUrl: './set-memories-activity.component.scss'
+})
+export class SetMemoriesActivityComponent {
+  quickActivity: string = '';
+}
---
+++ src/app/pages/set-memories-activity/set-memories-activity.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SetMemoriesActivityComponent } from './set-memories-activity.component';
+import { BackButtonModule } from "../../components/back-button/back-button.module";
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SetMemoriesActivityComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    SetMemoriesActivityComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    BackButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule,
+    FormsModule 
+],
+  providers: [],
+})
+export class SetMemoriesActivityModule { }

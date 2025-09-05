d344985f08ef454c5d28be57e26bf81ba239b333
Flo <florianhofer024@gmail.com>
2025-09-05 08:34:13 +0200

    progress activity filter

--- src/app/components/_dialogs/filter-bottom-sheet/filter-bottom-sheet.component.ts
+++ src/app/components/_dialogs/filter-bottom-sheet/filter-bottom-sheet.component.ts
@@ -1,75 +1,116 @@
-import { Component, Input, signal, WritableSignal } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
-import { ActivityService } from '../../../services/activity.service';
-import { MemoriseUserActivity } from '../../../models/activityInterface.model';
-import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { CommonModule } from '@angular/common';
+import { GeocoderResponse } from '../../../models/geocoder-response.model';
+import { MatDividerModule } from '@angular/material/divider';
+
+interface FilterData {
+  currentFilters: any;
+  currentLocation?: GeocoderResponse | null;
+}
 
 @Component({
   selector: 'app-filter-bottom-sheet',
+  standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatSliderModule,
-    MatSelectModule,
     ReactiveFormsModule,
+    MatButtonModule,
     MatFormFieldModule,
-    MatButtonModule
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatIconModule,
+    MatChipsModule,
+    MatCheckboxModule,
+    MatButtonToggleModule,
+    MatDividerModule
   ],
-  templateUrl: './filter-bottom-sheet.component.html',
-  styleUrl: './filter-bottom-sheet.component.scss'
+  templateUrl: 'filter-bottom-sheet.component.html',
+  styleUrl: 'filter-bottom-sheet.component.scss'
 })
-export class FilterBottomSheetComponent {
-
+export class FilterBottomSheetComponent implements OnInit {
   filterForm: FormGroup;
-  filteredActivities: WritableSignal<MemoriseUserActivity[]> = signal([]);
-  @Input() activities: MemoriseUserActivity[] = [];
+  currentLocation: GeocoderResponse | null = null;
+  private initialFormValue: any;
 
-
-  constructor(private fb: FormBuilder, private activityService: ActivityService) {
+  constructor(
+    private fb: FormBuilder,
+    private bottomSheetRef: MatBottomSheetRef<FilterBottomSheetComponent>,
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: FilterData,
+  ) {
     this.filterForm = this.fb.group({
       location: [''],
       distance: [25],
-      tag: [''],
-      groupSize: [0],
-      price: [100],
       season: [''],
       weather: [''],
-      name: ['']
+      activityType: [[]],
+      groupSize: [1],
+      price: [100],
+      freeActivities: [false],
+      familyFriendly: [false],
+      petFriendly: [false],
+      accessibleActivities: [false]
     });
   }
 
-  applyFilters(): void {
-    const filters = this.filterForm.value;
-    console.log(filters);
+  ngOnInit(): void {
+    if (this.data?.currentLocation) {
+      this.currentLocation = this.data.currentLocation;
+    }
 
-    this.activityService.getFilteredActivities(filters).subscribe({
-      next: (response) => {
-        this.filteredActivities.set(response);
-      },
-      error: (err) => {
-        console.error('Error fetching filterd activities', err);
-      }
-    });
+    // Set current filters if provided
+    if (this.data?.currentFilters) {
+      this.filterForm.patchValue(this.data.currentFilters);
+    }
+
+    // Store initial form value to detect changes
+    this.initialFormValue = this.filterForm.value;
+  }
+
+  useCurrentLocation(): void {
+    if (this.currentLocation) {
+      this.filterForm.patchValue({
+        location: this.currentLocation.results[0].formatted_address
+      });
+    }
+  }
+
+  hasChanges(): boolean {
+    return JSON.stringify(this.filterForm.value) !== JSON.stringify(this.initialFormValue);
   }
 
   resetFilters(): void {
     this.filterForm.reset({
       location: '',
       distance: 25,
-      tag: '',
-      groupSize: 0,
-      price: 100,
       season: '',
       weather: '',
-      name: ''
+      activityType: [],
+      groupSize: 1,
+      price: 100,
+      freeActivities: false,
+      familyFriendly: false,
+      petFriendly: false,
+      accessibleActivities: false
     });
-
-    this.filteredActivities.set([...this.activities]);
   }
 
+  applyFilters(): void {
+    this.bottomSheetRef.dismiss(this.filterForm.value);
+  }
+
+  dismiss(): void {
+    this.bottomSheetRef.dismiss();
+  }
 }
--- src/app/components/activity-list/activity-list.component.ts
+++ src/app/components/activity-list/activity-list.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, OnInit, signal, WritableSignal } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
@@ -11,6 +11,8 @@ import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MemoriseUserActivity } from '../../models/activityInterface.model';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ActivityCardComponent } from "../activity-card/activity-card.component";
@@ -22,6 +24,7 @@ import { GeocodingService } from '../../services/geocoding.service';
 import { GeocoderResponse } from '../../models/geocoder-response.model';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { FilterBottomSheetComponent } from '../_dialogs/filter-bottom-sheet/filter-bottom-sheet.component';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-activity-list',
@@ -42,6 +45,8 @@ import { FilterBottomSheetComponent } from '../_dialogs/filter-bottom-sheet/filt
     MatIconModule,
     MatSlideToggleModule,
     MatExpansionModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
     ActivityCardComponent,
     NotFoundComponent,
   ],
@@ -57,84 +62,209 @@ export class ActivityListComponent implements OnInit {
   pageSize = 12;
   currentPage = 0;
   filterForm: FormGroup;
+  searchControl = new FormControl('');
   fulladdress: GeocoderResponse | null = null;
+  isLoading = false;
 
-  constructor(private fb: FormBuilder, private router: Router, private activityService: ActivityService, private bookmarkedService: BookmarkService, private geocodingService: GeocodingService) {
+  private _bottomSheet = inject(MatBottomSheet);
+
+  constructor(
+    private fb: FormBuilder, 
+    private router: Router, 
+    private activityService: ActivityService, 
+    private bookmarkedService: BookmarkService, 
+    private geocodingService: GeocodingService
+  ) {
     this.filterForm = this.fb.group({
       location: [''],
       distance: [25],
-      tag: [''],
-      groupSize: [0],
-      price: [100],
       season: [''],
       weather: [''],
-      name: ['']
+      activityType: [[]],
+      groupSize: [1],
+      price: [100],
+      name: [''],
+      freeActivities: [false],
+      familyFriendly: [false],
+      petFriendly: [false],
+      accessibleActivities: [false]
     });
   }
 
   ngOnInit(): void {
     this.filteredActivities.set([...this.activities]);
-    this.paginatorLength = this.filteredActivities.length;
+    this.paginatorLength = this.filteredActivities().length;
     this.updatePaginatedActivities(this.activities);
     this.getCurrentLocation();
+    this.setupSearchSubscription();
   }
 
-  private getCurrentLocation() {
+  private getCurrentLocation(): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         async (position) => {
-          this.fulladdress = await this.geocodingService.geocodeLatLng({ lat: position.coords.latitude, lng: position.coords.longitude })
+          this.fulladdress = await this.geocodingService.geocodeLatLng({ 
+            lat: position.coords.latitude, 
+            lng: position.coords.longitude 
+          });
         },
         (error) => {
-          console.warn('Geolocation nicht verfügbar:', error);
+          console.warn('Geolocation not available:', error);
         }
       );
     }
   }
 
+  private setupSearchSubscription(): void {
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(searchTerm => {
+      this.filterForm.patchValue({ name: searchTerm });
+      this.applyFilters();
+    });
+  }
+
+  openFilterBottomSheet(): void {
+    const bottomSheetRef = this._bottomSheet.open(FilterBottomSheetComponent, {
+      data: {
+        currentFilters: this.filterForm.value,
+        currentLocation: this.fulladdress
+      },
+      panelClass: 'filter-bottom-sheet-panel'
+    });
+
+    bottomSheetRef.afterDismissed().subscribe(filters => {
+      if (filters) {
+        this.filterForm.patchValue(filters);
+        this.searchControl.setValue(filters.name || '', { emitEvent: false });
+        this.applyFilters();
+      }
+    });
+  }
+
   applyFilters(): void {
     const filters = this.filterForm.value;
-    console.log(filters);
+    this.isLoading = true;
 
     this.activityService.getFilteredActivities(filters).subscribe({
       next: (response) => {
         this.filteredActivities.set(response);
         this.currentPage = 0;
+        this.paginatorLength = this.filteredActivities().length;
         this.updatePaginatedActivities(this.filteredActivities());
+        this.isLoading = false;
       },
       error: (err) => {
-        console.error('Error fetching filterd activities', err);
+        console.error('Error fetching filtered activities', err);
+        this.isLoading = false;
       }
     });
-  }
-
-  private _bottomSheet = inject(MatBottomSheet);
-
-  openBottomSheet(): void {
-    this._bottomSheet.open(FilterBottomSheetComponent);
   }
 
   resetFilters(): void {
     this.filterForm.reset({
       location: '',
       distance: 25,
-      tag: '',
-      groupSize: 0,
-      price: 100,
       season: '',
       weather: '',
-      name: ''
+      activityType: [],
+      groupSize: 1,
+      price: 100,
+      name: '',
+      freeActivities: false,
+      familyFriendly: false,
+      petFriendly: false,
+      accessibleActivities: false
     });
 
+    this.searchControl.setValue('', { emitEvent: false });
     this.filteredActivities.set([...this.activities]);
     this.currentPage = 0;
+    this.paginatorLength = this.activities.length;
     this.updatePaginatedActivities(this.activities);
+  }
+
+  hasActiveFilters(): boolean {
+    const formValue = this.filterForm.value;
+    return !!(
+      formValue.location ||
+      formValue.season ||
+      formValue.weather ||
+      (formValue.activityType && formValue.activityType.length > 0) ||
+      formValue.groupSize > 1 ||
+      formValue.price < 100 ||
+      formValue.freeActivities ||
+      formValue.familyFriendly ||
+      formValue.petFriendly ||
+      formValue.accessibleActivities ||
+      this.searchControl.value
+    );
+  }
+
+  removeFilter(filterName: string): void {
+    const resetValues: any = {
+      location: '',
+      season: '',
+      weather: '',
+      activityType: [],
+      groupSize: 1,
+      price: 100,
+      freeActivities: false,
+      familyFriendly: false,
+      petFriendly: false,
+      accessibleActivities: false
+    };
+
+    if (Object.prototype.hasOwnProperty.call(resetValues, filterName)) {
+      this.filterForm.patchValue({ [filterName]: resetValues[filterName] });
+      this.applyFilters();
+    }
+  }
+
+  getSeasonDisplayName(season: string): string {
+    const seasonMap: Record<string, string> = {
+      spring: 'Spring',
+      summer: 'Summer',
+      fall: 'Fall',
+      winter: 'Winter'
+    };
+    return seasonMap[season] || season;
+  }
+
+  getWeatherDisplayName(weather: string): string {
+    const weatherMap: Record<string, string> = {
+      sunny: 'Sunny',
+      rainy: 'Rainy',
+      snowy: 'Snowy',
+      cloudy: 'Cloudy'
+    };
+    return weatherMap[weather] || weather;
+  }
+
+  getResultsCountText(): string {
+    const count = this.viewMode === 'bookmark' 
+      ? this.bookmarkedActivities().length 
+      : this.filteredActivities().length;
+    
+    if (count === 0) {
+      return 'No activities found';
+    } else if (count === 1) {
+      return '1 activity found';
+    } else {
+      return `${count} activities found`;
+    }
   }
 
   onPageChange(event: PageEvent): void {
     this.currentPage = event.pageIndex;
     this.pageSize = event.pageSize;
-    this.updatePaginatedActivities(this.activities);
+    
+    const activities = this.viewMode === 'bookmark' 
+      ? this.bookmarkedActivities()
+      : this.filteredActivities();
+    
+    this.updatePaginatedActivities(activities);
   }
 
   updatePaginatedActivities(activities: MemoriseUserActivity[]): void {
@@ -142,16 +272,18 @@ export class ActivityListComponent implements OnInit {
     this.paginatedActivities = activities.slice(startIndex, startIndex + this.pageSize);
   }
 
-  checkForBookmarking(viewMode: string) {
+  checkForBookmarking(viewMode: string): void {
     if (viewMode === 'bookmark') {
-      this.paginatorLength = this.bookmarkedActivities.length;
-    }
-    else {
+      this.paginatorLength = this.bookmarkedActivities().length;
+      this.updatePaginatedActivities(this.bookmarkedActivities());
+    } else {
       this.paginatorLength = this.filteredActivities().length;
+      this.updatePaginatedActivities(this.filteredActivities());
     }
+    this.currentPage = 0;
   }
 
-  navigateToDetails(activityId: number) {
+  navigateToDetails(activityId: number): void {
     this.router.navigate(['activity/details/', activityId.toString()]);
   }
 }

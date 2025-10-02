ebd0507733c4f307579463ef943783cac3ca94bc
fh2412 <florianhofer024@gmail.com>
2025-10-02 14:02:43 +0200

    progress issue #159: implemented frontend code for deleting photos

--- src/app/services/billing.service.ts
+++ src/app/services/billing.service.ts
@@ -1,11 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Observable, of } from 'rxjs';
+
+export interface DeletionData {
+    size: number;
+    userId: string;
+    path: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BillingService {
+
+
     private apiUrl = `${environment.apiUrl}/billing`;
 
 
@@ -15,8 +24,12 @@ export class BillingService {
     updateFreeUserStorageUsedAdd(user_id: string, data_size: number) {
         return this.http.put<any>(`${this.apiUrl}/freetier/${user_id}`, {data_size: data_size});
     }
-    updateFreeUserStorageUsedRemove(user_id: string, data_size: number) {
-        return this.http.put<any>(`${this.apiUrl}/freetier/${user_id}`, {data_size: data_size});
-    }
-
+    
+    updateStorageOnDeletion(dataArray: DeletionData[]): Observable<any> {
+        if (dataArray.length === 0) {
+          return of(void 0);
+        }
+    
+        return this.http.put(this.apiUrl, { deletions: dataArray });
+      }
 }
